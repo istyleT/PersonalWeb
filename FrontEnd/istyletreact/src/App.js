15dc3e4630db77145c1a02aa549e29f4fc1395da
I-style T <ajantug@gmail.com>
2023-04-24 15:35:39 +0700

    first commit

---
+++ FrontEnd/istyletreact/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import NavnarMain from './components/navbarmain';
+import FirstPage from './components/firstpage';
+
+function App() {
+  return (
+    <div className="App">
+         <NavnarMain />
+         <FirstPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ FrontEnd/istyletreact/src/components/navbarmain.js
@@ -0,0 +1,12 @@
+import './navbarmain.css';
+function NavbarMain () {
+                    return (
+                        <nav className="nav nav-masthead justify-content-center gap-3 text-light">
+                            <a className="nav-link active" href="#">I-styleT</a>
+                            <a className="nav-link" href="#">Applications</a>
+                            <a className="nav-link" href="#">Knowledge</a>
+                            <a className="nav-link" href="#">Contact</a>
+                        </nav>                       
+                    )
+}
+export default NavbarMain;
