e28372246746d795a1a7ddee83525bd24f4a65eb
I-style T <ajantug@gmail.com>
2023-04-27 19:39:34 +0700

    add : component form contact

---
+++ FrontEnd/istyletreact/src/pages/componentspage/formcontact.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './formcontact.css'
+const FormContact = () => {
+                    return (
+                                        <form className="main-formcontact">
+                                                            <div className="form-group"></div>
+                                        </form>
+                    );
+
+}
+export default FormContact;
