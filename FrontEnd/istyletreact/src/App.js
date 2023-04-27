576b1da7473068a2e4d82d7f3bfa46ce8dc7b373
I-style T <ajantug@gmail.com>
2023-04-27 16:02:04 +0700

    del : unuse redirect

--- FrontEnd/istyletreact/src/App.js
+++ FrontEnd/istyletreact/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Navbar from './components/Navbar';
-import { BrowserRouter, Routes, Route, redirect}
+import { BrowserRouter, Routes, Route}
     from 'react-router-dom';
 import Home from './pages';
 import Applications from './pages/applications';
